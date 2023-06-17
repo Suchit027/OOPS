d510b60620a4382144d474489402fe7779f370bd
Suchit Gupta <suchitgupta027@gmail.com>
2023-06-17 16:37:09 +0530

    examining how right shift operators affect the data

--- OOPS_JAVA/src/com/helloworld/HelloWorld.java
+++ OOPS_JAVA/src/com/helloworld/HelloWorld.java
@@ -1,9 +1,11 @@
 package com.helloworld;
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world.");
+		var rad = 2; // radius of circle
+		double area; // area of circle
+		area = 3.14 * rad * rad;
+		System.out.println("The are of circle is " + area);
+		
 	}
-	
 }
