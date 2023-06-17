22ca71cee45a0be54df7a1d1995b9d7ee7009e91
Suchit Gupta <suchitgupta027@gmail.com>
2023-06-17 18:54:16 +0530

    Learning how to create new arrays differently from C, boolean use, using string with switch

---
+++ OOPS_JAVA/src/com/helloworld/Boolean_demo.java
@@ -0,0 +1,15 @@
+package com.helloworld;
+
+public class Boolean_demo {
+	static public void main(String[] args) {
+		boolean b;
+		b = false;
+		System.out.println("b is " + b);
+		b = true;
+		System.out.println("b is " + b);
+		if (b) {
+			System.out.println("If b is true then this is printed");
+		}
+	}
+
+}
---
+++ OOPS_JAVA/src/com/helloworld/array_creation.java
@@ -0,0 +1,16 @@
+package com.helloworld;
+
+public class array_creation {
+	public static void main(String[] args) {
+		int[] arr = new int[5];
+		arr[0] = 0;
+		arr[1] = 1;
+		arr[2] = 2;
+		arr[3] = 3;
+		arr[4] = 4;
+		for (int i = 0; i < 5; ++i) {
+			System.out.println(arr[i]);
+		}
+		// array declaration could also have been done by arr[5];
+	}
+}
