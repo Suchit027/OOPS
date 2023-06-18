4b5ca32f5790b4a5fd74997a41dc46eab77037a3
Suchit Gupta <suchitgupta027@gmail.com>
2023-06-18 12:27:16 +0530

    understanding overloading

--- OOPS_JAVA/src/com/helloworld/Box_demo.java
+++ OOPS_JAVA/src/com/helloworld/Box_demo.java
@@ -6,12 +6,23 @@ class box {
 	double height;
 	double length;
 
-	public box(double w, double h, double l) { // making the constructor
-		width = w; // could have also used this.width; 'this' is used to refer to the object which invoked the method
+	box(double w, double h, double l) { // making the constructor
+		width = w; // could have also used this.width; 'this' is used to refer to the object which
+					// invoked the method
 		height = h;
 		length = l;
 	}
 
+	box() { // default constructor
+		width = -1;
+		height = -1;
+		length = -1;
+	}
+
+	box(double side) {// constructor for squares
+		width = length = height = side;
+	}
+	
 	double volume() {
 		double vol;
 		vol = width * height * length;
@@ -23,7 +34,7 @@ class box {
 public class Box_demo {
 	public static void main(String[] args) {
 		box mybox1 = new box(10, 10, 10);
-		box mybox2 = new box(11, 11, 11);
+		box mybox2 = new box(11);
 		System.out.println("Volume of box 1 is " + mybox1.volume());
 		System.out.println("Volume of box 2 is " + mybox2.volume());
 	}
