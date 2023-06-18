473948068ddf951d687ea3d3826692d20b8f1769
Suchit Gupta <suchitgupta027@gmail.com>
2023-06-18 16:04:32 +0530

    renaming the file

---
+++ OOPS_JAVA/src/com/helloworld/box.java
@@ -0,0 +1,41 @@
+package com.helloworld;
+
+public class box {
+
+	double width;
+	double height;
+	double length;
+
+	box(double w, double h, double l) { // making the constructor
+		width = w; // could have also used this.width; 'this' is used to refer to the object which
+					// invoked the method
+		height = h;
+		length = l;
+	}
+
+	box() { // default constructor
+		width = -1;
+		height = -1;
+		length = -1;
+	}
+
+	box(double side) {// constructor for squares
+		width = length = height = side;
+	}
+
+	double volume() {
+		double vol;
+		vol = width * height * length;
+		return vol;
+	}
+
+}
+
+class Box_demo {
+	public static void main(String[] args) {
+		box mybox1 = new box(10, 10, 10);
+		box mybox2 = new box(11);
+		System.out.println("Volume of box 1 is " + mybox1.volume());
+		System.out.println("Volume of box 2 is " + mybox2.volume());
+	}
+}
