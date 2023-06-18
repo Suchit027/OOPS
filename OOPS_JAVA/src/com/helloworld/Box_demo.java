dcb6922044c02ca7daae1b973be5a4f258f5b97b
Suchit Gupta <suchitgupta027@gmail.com>
2023-06-18 16:15:12 +0530

    renaming

--- OOPS_JAVA/src/com/helloworld/Box_demo.java
+++ OOPS_JAVA/src/com/helloworld/Box_demo.java
@@ -1,25 +1,25 @@
 package com.helloworld;
 
-public class Box_demo {
+class box {
 
 	double width;
 	double height;
 	double length;
 
-	Box_demo(double w, double h, double l) { // making the constructor
+	box(double w, double h, double l) { // making the constructor
 		width = w; // could have also used this.width; 'this' is used to refer to the object which
 					// invoked the method
 		height = h;
 		length = l;
 	}
 
-	Box_demo() { // default constructor
+	box() { // default constructor
 		width = -1;
 		height = -1;
 		length = -1;
 	}
 
-	Box_demo(double side) {// constructor for squares
+	box(double side) {// constructor for squares
 		width = length = height = side;
 	}
 
@@ -31,10 +31,10 @@ public class Box_demo {
 
 }
 
-class Box_demo {
+public class Box_demo {
 	public static void main(String[] args) {
-		Box_demo mybox1 = new Box_demo(10, 10, 10);
-		Box_demo mybox2 = new Box_demo(11);
+		box mybox1 = new box(10, 10, 10);
+		box mybox2 = new box(11);
 		System.out.println("Volume of box 1 is " + mybox1.volume());
 		System.out.println("Volume of box 2 is " + mybox2.volume());
 	}
